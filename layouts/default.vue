e7898a36ffb69b8ac07810370fd8ddc37f073d7f
steelpear <steelpear@gmail.com>
2020-04-22 07:32:18 +0300

    add cookie

--- layouts/default.vue
+++ layouts/default.vue
@@ -38,18 +38,58 @@
       color="indigo"
       dark
     >
-      <v-flex class="text-right overline">
+      <v-spacer />
+      <div class="overline">
         &copy; qr-generator {{ new Date().getFullYear() }}
-      </v-flex>
+      </div>
+      <v-bottom-sheet
+        v-model="cookiePolicy"
+        full-width
+        hide-overlay
+        persistent
+      >
+        <v-card class="py-2">
+          <v-row align="center" justify="center">
+            <v-row align="center" justify="center">
+              <v-icon large class="ml-3 mr-2 hidden-sm-and-down">
+                info
+              </v-icon>
+              <v-col :class="$vuetify.breakpoint.smAndDown ? 'text-xs-justify ml-4' : 'text-xs-center'" style="max-width: fit-content;">
+                На этом сайте используются файлы <span class="font-weight-bold">cookie</span>. Продолжая просматривать его, Вы соглашаетесь с этим. Чтобы узнать больше <nuxt-link to="/policy" class="indigo--text font-weight-bold">
+                  нажмите здесь
+                </nuxt-link>.
+              </v-col>
+            </v-row>
+            <v-btn depressed color="primary" class="mr-6" @click="cookieOk">
+              OK, понятно
+            </v-btn>
+          </v-row>
+        </v-card>
+      </v-bottom-sheet>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueCookies from 'vue-cookies'
+Vue.use(VueCookies)
 export default {
   data () {
     return {
-      title: 'Генератор QR-кода'
+      title: 'Генератор QR-кода',
+      cookiePolicy: true
+    }
+  },
+  created () {
+    if (this.$cookies.get('cookie_assent')) {
+      this.cookiePolicy = false
+    }
+  },
+  methods: {
+    cookieOk () {
+      this.cookiePolicy = false
+      this.$cookies.set('cookie_assent', 'cookie_session', '30d')
     }
   }
 }
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,15 +1,26 @@
 <template>
-  <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
-  </v-app>
+  <v-container>
+    <v-row align="center" justify="center" class="mt-5">
+      <v-col cols="12" md="12" xs="12" class="text-center">
+        <h1 class="text-xs-center display-4 font-weight-bold">
+          404
+        </h1>
+      </v-col>
+      <v-col cols="12" md="12" xs="12" class="text-center">
+        <h1 class="text-xs-center display-2">
+          Страница не найдена
+        </h1>
+      </v-col>
+      <v-col cols="12" md="12" xs="12" class="text-center">
+        <p class="text-xs-center title mt-4">
+          Сожалеем, но такой страницы не существует.
+        </p>
+      </v-col>
+      <v-btn large color="info mt-4" nuxt to="/">
+        Перейти на главную
+      </v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -36,9 +47,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-h1 {
-  font-size: 20px;
-}
-</style>
